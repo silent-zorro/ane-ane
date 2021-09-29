c008fbb36f7bb9062e97e60f473cb7fbf1fd3aec
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-09-29 21:12:52 +0530

    Add files via upload

---
+++ src/commands/General/Help.ts
@@ -0,0 +1,62 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'help',
+            description: 'Displays the help menu or shows the info of the command provided',
+            category: 'general',
+            usage: `${client.config.prefix}help (command_name)`,
+            aliases: ['h']
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+        if (!parsedArgs.joined) {
+            const commands = this.handler.commands.keys()
+            const categories: { [key: string]: ICommand[] } = {}
+            for (const command of commands) {
+                const info = this.handler.commands.get(command)
+                if (!command) continue
+                // if (!info?.config?.category || info.config.category === 'dev') continue
+                if (!info?.config?.category) continue
+                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
+                else {
+                    categories[info.config.category] = []
+                    categories[info.config.category].push(info)
+                }
+            }
+            let text = `🥰 *Cortana by ISURU* 🥰\n\n`
+            const keys = Object.keys(categories)
+            for (const key of keys)
+                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
+                    key
+                ]
+                    .map((command) => command.config?.command)
+                    .join(', ')}\`\`\`\n\n`
+            return void M.reply(
+                `${text} ❗️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info or contact Isuru using mods command*`
+            )
+        }
+        const key = parsedArgs.joined.toLowerCase()
+        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
+        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
+        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
+        M.reply(
+            `🎫 *Command:* ${this.client.util.capitalize(command.config?.command)}\n🐺 *Status:* ${
+                state ? 'Disabled' : 'Available'
+            }\n🔆 *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+                command.config.aliases
+                    ? `\n⚡️ *Features:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    : ''
+            }\n🃏 *Group Only:* ${this.client.util.capitalize(
+                JSON.stringify(!command.config.dm ?? true)
+            )}\n📌 *Usage:* ${command.config?.usage || ''}\n\n🔖 *Description:* ${command.config?.description || ''}`
+        )
+    }
+
+    emojis = ['👨‍💻', '👽', '🌌', '🎬', '👑', '💻', '😏', '😎', '💠']
+}
