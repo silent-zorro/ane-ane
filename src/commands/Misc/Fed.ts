fd3071dfabbac10391e16538fe2cc5ef4f5c8986
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-09-30 16:48:48 +0530

    Update Fed.ts

--- src/commands/Misc/Fed.ts
+++ src/commands/Misc/Fed.ts
@@ -11,7 +11,7 @@ export default class Command extends BaseCommand {
             command: 'fed',
             description: 'Displays the info',
             category: 'misc',
-            usage: `${client.config.prefix}void`,
+            usage: `Zoom Link`,
         })
     }
 
