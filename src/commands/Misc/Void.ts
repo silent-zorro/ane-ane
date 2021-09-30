f364aaad0604dae05f75617252d2685413087600
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-09-30 16:52:24 +0530

    Update Void.ts

--- src/commands/Misc/Void.ts
+++ src/commands/Misc/Void.ts
@@ -11,7 +11,7 @@ export default class Command extends BaseCommand {
             command: 'dev',
             description: 'Displays the info',
             category: 'misc',
-            usage: `${client.config.prefix}void`
+            usage: `Developer`
         })
     }
 
