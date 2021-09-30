c189f9903889a740198dac0db02a299facaeeac2
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-09-30 16:56:36 +0530

    Update play.ts

--- src/commands/Media/play.ts
+++ src/commands/Media/play.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
             description: '🎵 play a song with just search Key word',
             category: 'media',
             aliases: ['music'],
-            usage: `${client.config.prefix}play [term]`,
+            usage: `Play any song You Want`,
             baseXp: 30
         })
     }
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
                 contextInfo: {
                     externalAdReply: {
                         title: videos[0].title.substr(0, 30),
-                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Kaoi`,
+                        body: `author : ${videos[0].author.name.substr(0, 20)}\nSent Via : Cortana Saver`,
                         mediaType: 2,
                         thumbnailUrl: `https://i.ytimg.com/vi/${audio.id}/hqdefault.jpg`,
                         mediaUrl: audio.url
