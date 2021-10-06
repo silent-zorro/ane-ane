f947933c8775c6a68ea12c3bfd681ea15fe6fde4
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-10-06 23:07:07 +0530

    Update Ip.ts

--- src/commands/Fun/Ip.ts
+++ src/commands/Fun/Ip.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         await axios
-            .get(`https://freegeoip.app/json/`)
+            .get(`https://api.ipify.org/?format=json`)
             .then((response) => {
                 // console.log(response);
                 const text = `👨‍💻 *Ip:* ${response.data.ip}\n\n*🗺 Contry code:* ${response.data.country_code}\n\n*🌎 Country name:* ${response.data.country_name}\n\n*🌍 Region name:* ${response.data.region_name}\n\n*🌏 City name:* ${response.data.city}\n\n*⌚️ Time zone:* ${response.data.time_zone}`
