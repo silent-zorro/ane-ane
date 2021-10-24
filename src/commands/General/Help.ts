34554eddd5574164803135570d662accfdbda5d8
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-10-24 20:03:41 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -6,11 +6,11 @@ import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'help',
+            command: 'cortana',
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            aliases: ['h']
+            aliases: ['cortana']
         })
     }
 
