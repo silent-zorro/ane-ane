18c909267c1286dce548e989322c2c24df5fa0ec
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-10-24 20:24:21 +0530

    Update Chat.ts

--- src/commands/Misc/Chat.ts
+++ src/commands/Misc/Chat.ts
@@ -7,7 +7,7 @@ import axios from 'axios'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'chat',
+            command: 'c',
             description: 'Chat with Cortana AI by ISURU',
             aliases: ['Cortana'],
             category: 'misc',
