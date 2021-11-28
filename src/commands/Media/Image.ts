424a19a4035124980cb2ac24d2cface270e9402b
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-11-28 23:44:27 +0530

    Update Image.ts

--- src/commands/Media/Image.ts
+++ src/commands/Media/Image.ts
@@ -1,30 +1,51 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'advice',
-            description: 'Gives you random advice.',
-            category: 'fun',
-            usage: `${client.config.prefix}advice`,
-            baseXp: 30
+            command: 'image',
+            description: 'Will search img from the given word',
+            aliases: ['img'],
+            category: 'media',
+            usage: `${client.config.prefix}img`
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        await axios
-            .get(`https://api.adviceslip.com/advice`)
-            .then((response) => {
-                // console.log(response);
-                const text = `*Advice for you🔖:* ${response.data.slip.advice}`
-                M.reply(text)
-            })
-            .catch((err) => {
-                M.reply(`🔍 Error: ${err}`)
-            })
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void (await M.reply(`Please give a term to search`));
+        const search: any = joined.trim();
+        const term: string = search;
+        const { data } = await axios.get('https://imsea.herokuapp.com/api/1?q=${term}');
+        const buffer = await request.buffer(data.results).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `🕵️Generated for you by Isuru🕵️`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Try again or use the link. Here's the URL: ${data.results}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Try again or use the link. Here's the URL : ${data.results}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
     }
 }
