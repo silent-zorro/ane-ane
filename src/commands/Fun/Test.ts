d02ce45be78d4883217a28dd858f86808ecdb9e4
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-12-02 00:01:24 +0530

    Update Test.ts

--- src/commands/Fun/Test.ts
+++ src/commands/Fun/Test.ts
@@ -1,25 +1,29 @@
-import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
 import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'loli',
-            description: 'Will send you random loli image',
-            category: 'anime',
-            usage: `${client.config.prefix}loli`
+            command: 'test',
+            aliases: ['ss', 'ssweb'],
+            description: 'Gives you the screenshot of the given url. ',
+            category: 'fun',
+            usage: `${client.config.prefix}screenshot [url]`,
+            baseXp: 30
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        const onefive = Math.floor(Math.random() * 145) + 1
-
-        return void M.reply(
-            await request.buffer(`https://media.publit.io/file/Twintails/${onefive}.jpg`),
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void (await M.reply(`Please provide the url`))
+        const url = joined.trim()
+        
+        return void M.reply(response.data.message)
+            await request.buffer(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${url}`),
 
             MessageType.image,
             undefined,
