7e1a60a715de939c9dec8db93d4e25d8525f29e7
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-12-01 21:16:19 +0530

    Update Test.ts

--- src/commands/Fun/Test.ts
+++ src/commands/Fun/Test.ts
@@ -9,7 +9,7 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'catpic',
+            command: 'test',
             description: 'Will send you a cute cat image.',
             aliases: ['Meaw'],
             category: 'fun',
@@ -19,18 +19,20 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         // fetch result of https://zxbott.herokuapp.com/husbu from the API using axios
-        const { data } = await axios.get('https://aws.random.cat/meow')
-        const buffer = await request.buffer(data.file).catch((e) => {
+       if (!joined) return void M.reply('Please provide me your name.')
+        const place = joined.trim()
+        const { data } = await axios.get('https://nekobot.xyz/api/imagegen?type=changemymind&text=${place}')
+        const buffer = await request.buffer(data.message).catch((e) => {
             return void M.reply(e.message)
         })
         while (true) {
             try {
                 M.reply(
-                    buffer || 'Could not fetch image. Please try again later',
+                    buffer || 'code fucked isuru :-/',
                     MessageType.image,
                     undefined,
                     undefined,
-                    `Meaw  😻\n`,
+                    ` code fucking works isuru 😻\n`,
                     undefined
                 ).catch((e) => {
                     console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
