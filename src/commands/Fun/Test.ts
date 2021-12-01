ed0f75ae69ff4db63f172602fdba580c1265946e
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-12-01 21:12:00 +0530

    Add files via upload

---
+++ src/commands/Fun/Test.ts
@@ -0,0 +1,49 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'catpic',
+            description: 'Will send you a cute cat image.',
+            aliases: ['Meaw'],
+            category: 'fun',
+            usage: `${client.config.prefix}catpic`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        // fetch result of https://zxbott.herokuapp.com/husbu from the API using axios
+        const { data } = await axios.get('https://aws.random.cat/meow')
+        const buffer = await request.buffer(data.file).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `Meaw  😻\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Try again or use the link. Here's the URL: ${data.file}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Try again or use the link. Here's the URL : ${data.file}`)
+                console.log(`This error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
+}
