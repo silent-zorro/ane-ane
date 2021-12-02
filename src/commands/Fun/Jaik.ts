8fff3eb317d0a5563db6144746ed657811399537
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-12-02 14:08:00 +0530

    Update Jaik.ts

--- src/commands/Fun/Jaik.ts
+++ src/commands/Fun/Jaik.ts
@@ -1,15 +1,17 @@
-import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import jimp from 'jimp'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'blur',
+            command: 'jail',
             description: 'Blurs the given image or profile pic',
-            category: 'utils',
+            category: 'fun',
             usage: `${client.config.prefix}blur [(as caption | quote)[image] | @mention]`,
             baseXp: 30
         })
@@ -24,12 +26,15 @@ export default class Command extends BaseCommand {
             ? this.client.getProfilePicture(M.mentioned[0])
             : this.client.getProfilePicture(M.quoted?.sender || M.sender.jid))
         if (!image) return void M.reply(`Couldn't fetch the required Image`)
-        const level = joined.trim() || '5'
-        const img = await jimp.read(image as string)
-        img.blur(isNaN(level as unknown as number) ? 5 : parseInt(level))
-        img.getBuffer(`image/png`, (err, buffer) => {
-            if (err) return void M.reply(err?.message || `Couldn't blur the image`)
-            M.reply(buffer, MessageType.image)
-        })
+        return void M.reply(
+            await request.buffer(
+                `https://some-random-api.ml/canvas/jail?avatar=${image}`
+            ),
+            MessageType.image,
+            undefined,
+            undefined,
+            `🧐 This is the preview 🧐\n`,
+            undefined
+        ).catch((reason: any) => M.reply(`✖ An error occurred with cortana server. Please try again later. ${reason}`))
     }
 }
