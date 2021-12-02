a0bf7e2a531578a1f479b4e29f32746ead47d22e
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-12-02 13:31:49 +0530

    Update Jail.ts

--- src/commands/Fun/Jail.ts
+++ src/commands/Fun/Jail.ts
@@ -1,9 +1,10 @@
-import { MessageType } from '@adiwajshing/baileys'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
