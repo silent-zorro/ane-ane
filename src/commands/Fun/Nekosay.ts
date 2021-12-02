757b7c1cbeaf80140f13ec612dfdf3d2ce4c7181
silent-zorro <91269387+silent-zorro@users.noreply.github.com>
2021-12-02 12:04:20 +0530

    Add files via upload

---
+++ src/commands/Fun/Nekosay.ts
@@ -0,0 +1,55 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+// import { MessageType, Mimetype } from '@adiwajshing/baileys'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'test',
+            description: `Anime characters ;)\nType ${client.config.prefix}ac to check all available options`,
+            aliases: ['ac', 'achar'],
+            category: 'fun',
+            usage: `${client.config.prefix}ac (option)`,
+            baseXp: 20
+        })
+    }
+
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        // consider neko and kitsune in furry
+        
+        const term = joined.trim()
+
+        // fetch result of https://waifu.pics/api/sfw from the API using axios
+        const { data } = await axios.get(`https://nekobot.xyz/api/imagegen?type=changemymind&text=${term}`)
+        const buffer = await request.buffer(data.message).catch((e) => {
+            return void M.reply(e.message)
+        })
+        while (true) {
+            try {
+                M.reply(
+                    buffer || 'Could not fetch image. Please try again later',
+                    MessageType.image,
+                    undefined,
+                    undefined,
+                    `Here you go.\n`,
+                    undefined
+                ).catch((e) => {
+                    console.log(`This Error occurs when an image is sent via M.reply()\n Child Catch Block : \n${e}`)
+                    // console.log('Failed')
+                    M.reply(`Could not fetch image. Here's the URL: ${data.url}`)
+                })
+                break
+            } catch (e) {
+                // console.log('Failed2')
+                M.reply(`Could not fetch image. Here's the URL : ${data.url}`)
+                console.log(`This Error occurs when an image is sent via M.reply()\n Parent Catch Block : \n${e}`)
+            }
+        }
+        return void null
+    }
+}
